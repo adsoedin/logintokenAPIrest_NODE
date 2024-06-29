a262d8d99ea08ac2f540879a0a26249ea5b96aef
edinADSO <edinADSO@gmail.com>
2024-06-29 01:00:35 -0500

    Registro, validacion, login, token, actualizacion

---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { register, login, updateUser} = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+// Ruta protegida para actualizar datos del usuario
+router.put('/update', authMiddleware, updateUser);
+
+// Ruta protegida de ejemplo
+router.get('/protected', authMiddleware, (req, res) => {
+    res.status(200).json({ mensaje: 'Accediste a una ruta protegida', user: req.user });
+  });
+
+module.exports = router;
